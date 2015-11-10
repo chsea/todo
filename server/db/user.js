e599f59ab9e70152d10b0772037f18983459d815
Chelsea Du <chsea.du@gmail.com>
2015-11-10 08:58:40 -0500

    add pw encryption

--- server/db/user.js
+++ server/db/user.js
@@ -1,21 +1,29 @@
 'use strict';
 const Sequelize = require('sequelize');
+const crypto = require('crypto');
 
-// const hashPw = pw => {
-//
-// };
+const generateSalt = () => crypto.randomBytes(16).toString('base64');
+const hashPassword = (plainText, salt) => {
+    let hash = crypto.createHash('sha1');
+    hash.update(plainText);
+    hash.update(salt);
+    return hash.digest('hex');
+};
 
 module.exports = db => {
   const User = db.define('user', {
     name: { type: Sequelize.STRING },
     username: { type: Sequelize.STRING },
+    salt: { type: Sequelize.STRING },
     password: { type: Sequelize.STRING }
   }, {
     timestamps: false
   });
 
-  // User.beforeCreate(user => {
-  //
-  // });
+  User.beforeCreate(user => {
+    user.salt = generateSalt();
+    user.password = hashPassword(user.password, user.salt);
+  });
+
   return User;
 };
